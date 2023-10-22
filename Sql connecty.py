c0604c8ff6a3f59fdc1b21a724567d801e2ed4c7
Teflon Dervish <voltvilty45@gmail.com>
2023-10-22 16:03:30 +0500

    Add sql connection

---
+++ Sql connecty.py
@@ -0,0 +1,10 @@
+import pyodbc
+
+connection_to_db = pyodbc.connect(r'Driver={SQL Server};Server=WIN-6KLU4EI17UL;Database=test;Trusted_Connection=yes;')
+cursor = connection_to_db.cursor()
+cursor.execute('SELECT user_name, age FROM users')
+while 1:
+    row = cursor.fetchone()
+    if not row:
+        break
+    print(row.user_name, row.age)
