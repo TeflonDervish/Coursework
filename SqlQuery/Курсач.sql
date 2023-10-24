58215f05d0b624d08720595343f0119a87884cd1
TeflonDervish <voltvilty45@gmail.com>
2023-10-24 22:28:05 +0500

    Add database

---
+++ SqlQuery/Курсач.sql
@@ -0,0 +1,43 @@
+create database TrampolineCenter;
+
+create table Visitors(
+	Visitor_ID int primary key,
+	Surname varchar(20),
+	Name varchar(20),
+	PhoneNumber varchar(15),
+	Email varchar(25)
+)
+
+create table Staff(
+	Staff_ID int primary key,
+	Surname varchar(20),
+	Name varchar(20),
+	MiddleName varchar(20),
+	PhoneNumber varchar(15),
+	LaborBookName int,
+	MedicalBookName int,
+)
+create table Equipment(
+	Equipment_ID int primary key,
+	Name varchar(20),
+	Purpose varchar(300),
+)
+
+create table Service(
+	Service_ID int primary key,
+	Price decimal(8, 2),
+	ServiceDescription varchar(300),
+	Limitations varchar(300),
+	RoomNumber tinyint, 
+	Equipment_ID int foreign key references Equipment(Equipment_ID),
+)
+
+create table PurchasedService(
+	PurchasedService_ID int primary key,
+	Visitor_ID int foreign key references Visitors(Visitor_ID),
+	Staff_ID int foreign key references Staff(Staff_ID),
+	Service_ID int foreign key references Service(Service_ID),
+	Price decimal(8, 2),
+	StartTime smalldatetime, 
+	EndTime smalldatetime,
+)
