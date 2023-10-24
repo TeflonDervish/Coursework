89af54fdfc1b39a7d4114f95462cea16d947c852
Teflon Dervish <voltvilty45@gmail.com>
2023-10-24 22:27:25 +0500

    Add forms

---
+++ CompileForms.sh
@@ -0,0 +1,3 @@
+pyuic5 -o Forms/LoginWindow.py Layout/LoginWindow.ui
+pyuic5 -o Forms/RegWindow.py Layout/RegWindow.ui
+pyuic5 -o Forms/AdminWindow.py Layout/AdminWindow.ui
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from PyQt5.QtWidgets import *
 import sys
-from Forms.Calc import Ui_MainWindow
+
+from Forms.LoginWindow import Ui_LoginWindow
 
 
-class MainWindow(QMainWindow, Ui_MainWindow): # главное окно
-
-
+class MainWindow(QMainWindow, Ui_LoginWindow): # главное окно
 
     def __init__(self, parent=None):
         super().__init__()
@@ -16,40 +15,34 @@ class MainWindow(QMainWindow, Ui_MainWindow): # главное окно
         self.add_function()
 
     def add_function(self):
-        self.push0.clicked.connect(lambda: self.PushNumber(self.push0.text()))
-        self.push1.clicked.connect(lambda: self.PushNumber(self.push1.text()))
-        self.push2.clicked.connect(lambda: self.PushNumber(self.push2.text()))
-        self.push3.clicked.connect(lambda: self.PushNumber(self.push3.text()))
-        self.push4.clicked.connect(lambda: self.PushNumber(self.push4.text()))
-        self.push5.clicked.connect(lambda: self.PushNumber(self.push5.text()))
-        self.push6.clicked.connect(lambda: self.PushNumber(self.push6.text()))
-        self.push7.clicked.connect(lambda: self.PushNumber(self.push7.text()))
-        self.push8.clicked.connect(lambda: self.PushNumber(self.push8.text()))
-        self.push9.clicked.connect(lambda: self.PushNumber(self.push9.text()))
+        self.pushLogin.clicked.connect(self.pressLogin)
 
-        self.pushAdd.clicked.connect(lambda: self.PushNumber(self.pushAdd.text()))
-        self.pushDel.clicked.connect(lambda: self.PushNumber(self.pushDel.text()))
-        self.pushDiff.clicked.connect(lambda: self.PushNumber(self.pushDiff.text()))
-        self.pushMul.clicked.connect(lambda: self.PushNumber(self.pushMul.text()))
+    def pressLogin(self):
+        error = QMessageBox()
 
-        self.pushEqual.clicked.connect(self.results)
+        date = QDateEdit
+        date.exec_()
+        error.setWindowTitle("Ошибка")
+        error.setText("Сейчас нельзя")
+        error.setIcon(QMessageBox.Warning)
+        error.setStandardButtons(QMessageBox.Cancel|QMessageBox.Ok)
 
-    def PushNumber(self, number):
-        if self.Result.text() == "0" or self.is_eq:
-            self.Result.setText(number)
-            self.is_eq = False
-        else:
-            self.Result.setText(self.Result.text() + number)
+        error.setDefaultButton(QMessageBox.Ok)
+        error.setInformativeText("Пизда тебе")
+        error.setDetailedText("Тикай с городу")
 
-    def results(self):
-        res = eval(self.Result.text())
-        self.Result.setText(str(res))
-        self.is_eq = True
+        error.buttonClicked.connect(self.popup_action)
 
+        error.exec_()
+
+    def popup_action(self, btn):
+        if btn.text() == 'Ok':
+            print("Зоебись")
+        elif btn.text() == 'Reset':
+            self.pushLogin.setText("Пизда тебе")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     win = MainWindow()
     win.show()
     sys.exit(app.exec_())
-    print(1)
