7bee64e5a1e4e3c2c80260b8693f612d68e8c9a2
TeflonDervish <voltvilty45@gmail.com>
2023-10-28 23:53:26 +0500

    Add dop tasks

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ import sys
 
 from Forms.LoginWindow import Ui_LoginWindow
 
-<<<<<<< HEAD
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     log = Ui_LoginWindow()
     log.show()
     sys.exit(log.exec_())
-=======
+
 class Window1(QMainWindow):
     def __init__(self):
         super(Window1, self).__init__()
@@ -26,4 +25,3 @@ if __name__ == "__main__":
     ui.setupUi(win)
     win.show()
     sys.exit(app.exec_())
->>>>>>> 28134ae24f4eadadc84bdf57ee7740b0d16d2a30
