76b4492cc3d5a7c4a3b580f8be38bced3936c6cc
Teflon Dervish <voltvilty45@gmail.com>
2023-10-28 22:33:25 +0500

    Test commit

--- Forms/AdminWindow.py
+++ Forms/AdminWindow.py
@@ -14,7 +14,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(848, 500)
+        MainWindow.resize(848, 499)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.tabWidget = QtWidgets.QTabWidget(self.centralwidget)
@@ -167,7 +167,7 @@ class Ui_MainWindow(object):
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
-        self.tabWidget.setCurrentIndex(0)
+        self.tabWidget.setCurrentIndex(2)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
--- main.py
+++ main.py
@@ -3,46 +3,19 @@ import sys
 
 from Forms.LoginWindow import Ui_LoginWindow
 
+class Window1(QMainWindow):
+    def __init__(self):
+        super(Window1, self).__init__()
 
-class MainWindow(QMainWindow, Ui_LoginWindow): # главное окно
+        self.setWindowTitle("Редактор кода")
+        self.move(200, 200)
+        self.setFixedSize(400, 400)
 
-    def __init__(self, parent=None):
-        super().__init__()
-        self.setupUi(self)
-
-        self.is_eq = False
-
-        self.add_function()
-
-    def add_function(self):
-        self.pushLogin.clicked.connect(self.pressLogin)
-
-    def pressLogin(self):
-        error = QMessageBox()
-
-        date = QDateEdit
-        date.exec_()
-        error.setWindowTitle("Ошибка")
-        error.setText("Сейчас нельзя")
-        error.setIcon(QMessageBox.Warning)
-        error.setStandardButtons(QMessageBox.Cancel|QMessageBox.Ok)
-
-        error.setDefaultButton(QMessageBox.Ok)
-        error.setInformativeText("Пизда тебе")
-        error.setDetailedText("Тикай с городу")
-
-        error.buttonClicked.connect(self.popup_action)
-
-        error.exec_()
-
-    def popup_action(self, btn):
-        if btn.text() == 'Ok':
-            print("Зоебись")
-        elif btn.text() == 'Reset':
-            self.pushLogin.setText("Пизда тебе")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    win = MainWindow()
+    win = QWidget
+    ui = Ui_LoginWindow()
+    ui.setupUi(win)
     win.show()
     sys.exit(app.exec_())
